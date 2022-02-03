05d06d4487df7ebd970cad1d44f46ef9db94c4a4
gvpodev <gavapim@gmail.com>
2022-02-03 19:35:52 -0300

    paginating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
 import { InputTextModule } from 'primeng/inputtext'
 import { ButtonModule } from 'primeng/button'
 import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { TableModule } from 'primeng/table';
     // Prime NG
     InputTextModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
