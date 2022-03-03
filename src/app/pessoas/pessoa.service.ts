8c64c53d8c74415340537eb80f51babb03b8cbe9
gvpodev <gavapim@gmail.com>
2022-03-03 11:56:29 -0300

    cadastro pessoa

--- src/app/pessoas/pessoa.service.ts
+++ src/app/pessoas/pessoa.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ErrorHandlerService } from '../core/error-handler.service';
+import { Pessoa } from '../core/model';
 
 export class PessoaFiltro {
   nome?: string
@@ -13,7 +14,7 @@ export class PessoaFiltro {
 })
 export class PessoaService {
 
-  private readonly BEARER_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDYzMzQ3NjEsInVzZXJfbmFtZSI6ImFkbWluQGFsZ2Ftb25leS5jb20iLCJhdXRob3JpdGllcyI6WyJST0xFX0NBREFTVFJBUl9DQVRFR09SSUEiLCJST0xFX1BFU1FVSVNBUl9QRVNTT0EiLCJST0xFX1JFTU9WRVJfUEVTU09BIiwiUk9MRV9DQURBU1RSQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUEVTUVVJU0FSX0xBTkNBTUVOVE8iLCJST0xFX1JFTU9WRVJfTEFOQ0FNRU5UTyIsIlJPTEVfQ0FEQVNUUkFSX1BFU1NPQSIsIlJPTEVfUEVTUVVJU0FSX0NBVEVHT1JJQSJdLCJqdGkiOiI4ejBBSWk3aFFUcWd5eWNkcnFzeTlPMndVU3ciLCJjbGllbnRfaWQiOiJhbmd1bGFyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl19.noQ6fGLK1zs5djzgQykn_nmH0zsqqCPttjD3fJqh8lM'
+  private readonly BEARER_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDYzMzczNDYsInVzZXJfbmFtZSI6ImFkbWluQGFsZ2Ftb25leS5jb20iLCJhdXRob3JpdGllcyI6WyJST0xFX0NBREFTVFJBUl9DQVRFR09SSUEiLCJST0xFX1BFU1FVSVNBUl9QRVNTT0EiLCJST0xFX1JFTU9WRVJfUEVTU09BIiwiUk9MRV9DQURBU1RSQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUEVTUVVJU0FSX0xBTkNBTUVOVE8iLCJST0xFX1JFTU9WRVJfTEFOQ0FNRU5UTyIsIlJPTEVfQ0FEQVNUUkFSX1BFU1NPQSIsIlJPTEVfUEVTUVVJU0FSX0NBVEVHT1JJQSJdLCJqdGkiOiJmcUtzVmk3c2pDV1VaQzN4cXhkdUY1aEd4LVUiLCJjbGllbnRfaWQiOiJhbmd1bGFyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl19._7I2BTXd3bfA_BZJVh4VCLIdC-DaolcBuLazqWAb1K4'
 
   pessoasUrl = 'http://localhost:8080/pessoas'
 
@@ -75,5 +76,16 @@ export class PessoaService {
       .toPromise()
       .then()
       .catch(err => this.errorHandler.handle(err))
+  }
+
+
+  adicionar(pessoa: Pessoa): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Authorization', this.BEARER_TOKEN)
+      .append('Content-Type', 'application/json')
+
+    return this.http.post(this.pessoasUrl, pessoa, { headers })
+      .toPromise()
+      .then(response => response);
   }
 }
--- src/app/pessoas/pessoas-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoas/pessoas-cadastro/pessoa-cadastro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { Pessoa } from 'src/app/core/model';
+import { PessoaService } from '../pessoa.service';
 
 @Component({
   selector: 'app-pessoa-cadastro',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoaCadastroComponent implements OnInit {
 
-  constructor() { }
+  pessoa = new Pessoa()
+
+  constructor(
+    private pessoaService: PessoaService,
+    private messageService: MessageService,
+    private errorHandler: ErrorHandlerService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+  salvar(form: NgForm) {
+    this.pessoaService.adicionar(this.pessoa)
+      .then(() => {
+        this.messageService.add({ severity: 'success', summary: 'Pessoa cadastrada', detail: 'Pessoa cadastrada com sucesso!' });
+
+        form.reset();
+        this.pessoa = new Pessoa();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
 }
